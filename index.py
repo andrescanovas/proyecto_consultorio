7a9e8c3dc8af997074b0017006eec221c92c246a
andres <andrescanovas92@gmail.com>
2021-11-03 00:26:13 -0300

    ventana emergente de formulario con toplevel

--- index.py
+++ index.py
@@ -1,5 +1,16 @@
 from tkinter import *
+from form import *
 
 root = Tk()
-root = title("Sanatorio paso al infierno")
+root.title("Sanatorio paso al infierno")
+root.geometry("600x400")
 
+
+
+
+# entrada = Entry(root, width=20)
+# entrada.grid(row=0)
+
+crearusuario = Button(root,text="Alta",command=alta_de_paciente).grid(row=1)
+
+mainloop()
