41d9e6c16b6267beeec379da9adc4a277522388b
andres <andrescanovas92@gmail.com>
2021-11-15 17:53:19 -0300

    Boton de eliminar

--- index.py
+++ index.py
@@ -13,7 +13,7 @@ from turnos import turnos
 # Desarrollo de la Interfaz grafica
 root=Tk()
 root.title("Base de Datos de pacientes")
-root.geometry("1024x640")
+root.geometry("1200x640")
 
 
 miId=StringVar()
@@ -23,6 +23,12 @@ miEdad=StringVar()
 miDNI =StringVar()
 miTelefono=StringVar()
 miEmail = StringVar()
+miCalle = StringVar()
+miAltura = StringVar()
+miCiudad = StringVar()
+miProvincia = StringVar()
+miPais = StringVar()
+miMedicocabe = StringVar()
 
 def salirAplicacion():
 	valor=messagebox.askquestion("Salir","¿Está seguro que desea salir de la Aplicación?")
@@ -37,6 +43,12 @@ def limpiarCampos():
 	miDNI.set("")
 	miTelefono.set("")
 	miEmail.set("")
+	miCalle.set("")
+	miAltura.set("")
+	miCiudad.set("")
+	miProvincia.set("") 
+	miPais.set("") 
+	miMedicocabe.set("")
 	
 def mensaje():
 	acerca='''
@@ -76,28 +88,28 @@ def seleccionarUsandoClick(event):
 tree.bind("<Double-1>", seleccionarUsandoClick)
 
 
-def mostrar(tipo):
+# def mostrar(tipo):
 
-	""" En esta funcion se muestran los registros de las tablas "paciente" y "medico" de la base de datos bd """
-	miConexion=sqlite3.connect("bd.db")
-	miCursor=miConexion.cursor()
-	registros=tree.get_children()
-	for elemento in registros:
-		tree.delete(elemento)
+# 	""" En esta funcion se muestran los registros de las tablas "paciente" y "medico" de la base de datos bd """
+# 	miConexion=sqlite3.connect("bd.db")
+# 	miCursor=miConexion.cursor()
+# 	registros=tree.get_children()
+# 	for elemento in registros:
+# 		tree.delete(elemento)
 
-	try:
-		if tipo==0:
-			tipo1="pacientes"
+# 	try:
+# 		if tipo==0:
+# 			tipo1="pacientes"
 			
-			ltipos.config(text='PACIENTES')
-		else:
-			tipo1="medicos"
-			ltipos.config(text='MEDICOS')
-		miCursor.execute("SELECT * FROM " + tipo1)
-		for row in miCursor:
-			tree.insert("",0,text=row[0], values=(row[1],row[2],row[3],row[4],row[5],row[6]))
-	except:
-		pass
+# 			ltipos.config(text='PACIENTES')
+# 		else:
+# 			tipo1="medicos"
+# 			ltipos.config(text='MEDICOS')
+# 		miCursor.execute("SELECT * FROM " + tipo1)
+# 		for row in miCursor:
+# 			tree.insert("",0,text=row[0], values=(row[1],row[2],row[3],row[4],row[5],row[6]))
+# 	except:
+# 		pass
 
 def actualizar():
 	miConexion=sqlite3.connect("bd.db")
@@ -123,18 +135,26 @@ def actualizar():
 	
 
 
-def borrar():
+def borrar(tipo):
 	miConexion=sqlite3.connect("bd.db")
 	miCursor=miConexion.cursor()
 	try:
+		if ltipos.cget("text") == "PACIENTES":
+			tipo1="pacientes"
+			tipo2=0
+		else:
+			tipo1="medicos"
+			tipo2=1
+			
+
 		if messagebox.askyesno(message="¿Realmente desea eliminar el registro?", title="ADVERTENCIA"):
-			miCursor.execute("DELETE FROM pacientes WHERE ID="+miId.get())
+			miCursor.execute("DELETE FROM "+tipo1+" WHERE ID="+miId.get())
 			miConexion.commit()
 	except:
-		messagebox.showwarning("ADVERTENCIA","Ocurrió un error al tratar de eliminar el registro")
+		messagebox.showwarning("ADVERTENCIA","No se pudo eliminar")
 		pass
 	limpiarCampos()
-	mostrar()
+	mostrar(tipo2)
 
 ###################### Colocar widgets en la VISTA ######################
 ########## Creando Los menus ###############
@@ -153,57 +173,66 @@ menubar.add_cascade(label="Ayuda",menu=ayudamenu)
 ############## Creando etiquetas y cajas de texto ###########################
 e1=Entry(root, textvariable=miId)
 
-# l2=Label(root, text="Nombre")
-# l2.place(x=20,y=10)
-# e2=Entry(root, textvariable=miNombre)
-# e2.place(x=80, y=10)
-
-# l3=Label(root, text="Apellido")
-# l3.place(x=250,y=10)
-# e3=Entry(root, textvariable=miApellido)
-# e3.place(x=320, y=10)
-
-# l4=Label(root, text="Edad")
-# l4.place(x=450,y=10)
-# e4=Entry(root, textvariable=miEdad, width=10)
-# e4.place(x=500, y=10)
-
-# l5=Label(root, text="DNI")
-# l5.place(x=600,y=10)
-# e5=Entry(root, textvariable=miDNI, width=10)
-# e5.place(x=640, y=10)
-
-
-
 l2=Label(root, text="Nombre")
-l2.grid(pady=10,row=1, column=0)
+l2.grid(pady=5,row=1, padx=10, column=1)
 e2=Entry(root, textvariable=miNombre)
-e2.grid(pady=10,row=1, column=1)
+e2.grid(pady=5,row=1, column=2)
 
 l3=Label(root, text="Apellido")
-l3.grid(pady=10,row=1, column=2)
+l3.grid(row=1, column=3)
 e3=Entry(root, textvariable=miApellido)
-e3.grid(pady=10,row=1, column=3)
+e3.grid(row=1, column=4)
 
 l4=Label(root, text="Edad")
-l4.grid(pady=10,row=1, column=4)
-e4=Entry(root, textvariable=miEdad, width=10)
-e4.grid(pady=10,row=1, column=5)
+l4.grid(row=1, column=5)
+e4=Entry(root, textvariable=miEdad)
+e4.grid(row=1, column=6)
 
 l5=Label(root, text="DNI")
-l5.grid(pady=10,row=1, column=6)
-e5=Entry(root, textvariable=miDNI, width=10)
-e5.grid(pady=10,row=1, column=7) 
+l5.grid(row=1, column=7)
+e5=Entry(root, textvariable=miDNI)
+e5.grid(row=1, column=8) 
 
 l6=Label(root, text="Telefono")
-l6.grid(row=2, column=0)
+l6.grid(pady=5,row=2, padx=10,column=1)
 e6=Entry(root, textvariable=miTelefono)
-e6.grid(row=2, column=1) 
+e6.grid(pady=5,row=2, column=2) 
 
 l7=Label(root, text="E-mail")
-l7.grid(row=2, column=2)
+l7.grid(row=2, column=3)
 e7=Entry(root, textvariable=miEmail)
-e7.grid(row=2, column=3) 
+e7.grid(row=2, column=4) 
+
+l8=Label(root, text="Calle")
+l8.grid(row=2, column=5)
+e8=Entry(root, textvariable=miCalle)
+e8.grid(row=2, column=6) 
+
+l9=Label(root, text="Altura")
+l9.grid(row=2, column=7)
+e9=Entry(root, textvariable=miAltura)
+e9.grid(row=2, column=8) 
+
+#-------------------------------
+l10=Label(root, text="Ciudad")
+l10.grid(row=3, padx=10,column=1)
+e10=Entry(root, textvariable=miCiudad)
+e10.grid(row=3, column=2) 
+
+l11=Label(root, text="Provincia")
+l11.grid(row=3, column=3)
+e11=Entry(root, textvariable=miProvincia)
+e11.grid(row=3, column=4) 
+
+l12=Label(root, text="Pais")
+l12.grid(row=3, column=5)
+e12=Entry(root, textvariable=miPais)
+e12.grid(row=3, column=6)
+
+l13=Label(root, text="Medico Cabecera")
+l13.grid(row=1, column=9)
+e13=Entry(root, textvariable=miMedicocabe)
+e13.grid(row=1, column=10) 
 
 
 
@@ -231,25 +260,25 @@ def crear_turno():
 ################# Creando botones ###########################
 
 b1=Button(root, text="Crear Registro", command=alta_de_paciente)
-b1.place(x=20, y=90)
+b1.place(x=20, y=120)
 b2=Button(root, text="Modificar Registro", command=actualizar)
-b2.place(x=145, y=90)
+b2.place(x=145, y=120)
 b3=Button(root, text="Mostrar paciente", command=lambda:mostrar(0))
-b3.place(x=20, y=125)
+b3.place(x=20, y=160)
 
-b4=Button(root, text="Eliminar Registro",bg="red", command=borrar)
-b4.place(x=300, y=90)
+b4=Button(root, text="Eliminar Registro",bg="red", command=lambda:borrar(0))
+b4.place(x=300, y=120)
 b5=Button(root, text="Mostrar medico", command=lambda:mostrar(1))
-b5.place(x=160, y=125)
+b5.place(x=160, y=160)
 b6=Button(root,text="CREAR TURNO",command=crear_turno)
-b6.place(x=550, y=125)
+b6.place(x=900, y=160)
 
 
 
 ################ LABEL DONDE SE MUESTRA SI EN LA LISTA HAY PACIENTE O MEDICOS "LABEL TIPOS: este label muestra si es medicos y pacientes"############################33
 
 ltipos=Label(root, text="",)
-ltipos.place(x=300,y=170)
+ltipos.place(x=500,y=170)
 
 
 def mostrar(tipo):
