98649a8fe9fbe5238c13e31e4f4b9f7ba0d7d50b
andres <andrescanovas92@gmail.com>
2021-11-15 20:26:36 -0300

    dfsgsdfgsdfg

--- index.py
+++ index.py
@@ -88,7 +88,7 @@ def seleccionarUsandoClick(event):
 	miCiudad.set(tree.item(item,"values")[8])
 	miProvincia.set(tree.item(item,"values")[9])
 	miPais.set(tree.item(item,"values")[10])
-	miMedicocabe.set(tree.item(item,"values")[11])
+	
 
 
 tree.bind("<Double-1>", seleccionarUsandoClick)
@@ -106,8 +106,8 @@ def actualizar():
 			tipo1="medicos"
 			tipo2=1
 
-		datos=miNombre.get(),miApellido.get(),miEdad.get(),miDNI.get(),miTelefono.get(),miEmail.get(),miCalle.get(),miAltura.get(),miCiudad.get(),miProvincia.get(),miPais.get(),miMedicocabe.get()
-		miCursor.execute("UPDATE "+ tipo1 +" SET NOMBRE=?, Apellido=?, Edad=?, dni=?, telefono=?, email=?, calle=?, altura=?, ciudad=?, provincia=?, pais=?, medicoCabe=? WHERE ID="+miId.get(), (datos))
+		datos=miNombre.get(),miApellido.get(),miEdad.get(),miDNI.get(),miTelefono.get(),miEmail.get(),miCalle.get(),miAltura.get(),miCiudad.get(),miProvincia.get(),miPais.get()
+		miCursor.execute("UPDATE "+ tipo1 +" SET NOMBRE=?, Apellido=?, Edad=?, dni=?, telefono=?, email=?, calle=?, altura=?, ciudad=?, provincia=?, pais=? WHERE ID="+miId.get(), (datos))
 		
 		miConexion.commit()
 	except:
@@ -212,10 +212,6 @@ l12.grid(row=3, column=5)
 e12=Entry(root, textvariable=miPais)
 e12.grid(row=3, column=6)
 
-l13=Label(root, text="Medico Cabecera")
-l13.grid(row=1, column=9)
-e13=Entry(root, textvariable=miMedicocabe)
-e13.grid(row=1, column=10) 
 
 
 
@@ -283,7 +279,7 @@ def mostrar(tipo):
 			ltipos.config(text='MEDICOS')
 		miCursor.execute("SELECT * FROM " + tipo1)
 		for row in miCursor:
-			tree.insert("",0,text=row[0], values=(row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8],row[9],row[10],row[11],row[12]))
+			tree.insert("",0,text=row[0], values=(row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8],row[9],row[10],row[11]))
 	except:
 		pass
 
